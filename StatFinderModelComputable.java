cdee8dd320bd038b46c6b06ef81daa928ba1d26a
Will Lehman <william.p.lehman@usace.army.mil>
2022-04-13 19:42:34 -0400

    testing

--- StatFinderModelComputable.java
+++ StatFinderModelComputable.java
@@ -1,3 +1,8 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class StatFinderModelComputable {
    public ModelLinks Linkages;
    public StatFinderModel Model;
@@ -12,15 +17,41 @@ public class StatFinderModelComputable {
                    //fetch the input
                    //where is the input?
                    String path = "/workspaces/config/" + li.input.name + ".csv";
+                   //create a destination array
+                   ArrayList<Double> flowlist = new ArrayList<Double>();
                    //read the file
-
+                    try {
+                        File input = new File(path);
+                        Scanner myReader = new Scanner(input);
+                        myReader.nextLine();//skip header.
+                        while (myReader.hasNextLine()) {
+                            String data = myReader.nextLine();
+                            String[] parts = data.split(",");
+                            double flow = Double.parseDouble(parts[1]);
+                            flowlist.add(flow);
+                            System.out.println(flow);
+                        }
+                        myReader.close();
+                    } catch (FileNotFoundException e) {
+                        System.out.println("An error occurred.");
+                        e.printStackTrace();
+                    }
                    //compute the stat?
+                   double stat = 0.0;
                    switch (o.parameter){
-                       case "max":
+                       case "max"://compute max
+                       stat = Double.MIN_VALUE;
+                       for(Double d : flowlist){
+                           if(d>stat) stat = d;
+                       }
                        break;
                        default:
-                       //compute max
+                       stat = Double.MIN_VALUE;
+                       for(Double d : flowlist){
+                           if(d>stat) stat = d;
+                       }
                    }
+                   //write output
                }
            }
        }
