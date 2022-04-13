3d68b0c30a17e312f71cf3ffe914dcb2557bb6ee
Will Lehman <william.p.lehman@usace.army.mil>
2022-04-12 20:21:17 -0400

    hello world

---
+++ javaplugin.java
@@ -0,0 +1,11 @@
+import java.io.Console;
+
+public class javaplugin  {
+    public static final String PluginName = "javaplugin";
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println(PluginName + " says hello.");
+    }
+}
